a5e0afac07f7b6ebbb1908c9ba2bbff1918885b3
CurtLiao <vermouth0226@gmail.com>
2019-04-17 00:09:03 +0800

    Display the list of customers and movies

---
+++ Vidly/Controllers/CustomerController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+
+            return View(customers);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var customers = GetCustomers();
+            var returnCustomer = new Customer();
+
+            foreach (var customer in customers) {
+                if (customer.Id == id) {
+                    returnCustomer = customer;
+                }
+            }
+
+            return View(returnCustomer);
+
+        }
+
+        public IEnumerable<Customer> GetCustomers()
+        {
+            var customers = new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John" },
+                new Customer { Id = 2, Name = "Jerry" }
+            };
+
+            return customers;
+        }
+    }
+}
---
+++ Vidly/Controllers/MovieController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            //var movies = new List<Movie>
+            //{
+            //    new Movie { Id = 1, Name = "Shrek" },
+            //    new Movie { Id = 2, Name = "Star Wars" }
+            //};
+
+            //var viewModel = new RandomMovieViewModel
+            //{
+            //    Movies = movies
+            //};
+            var movies = GetMovies();
+            
+            return View(movies);
+        }
+
+        public IEnumerable<Movie> GetMovies()
+        {
+            var movies = new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Star Wars" }
+            };
+
+            return movies;
+        }
+    }
+}
